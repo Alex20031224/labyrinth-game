af601c875bfa5313443c02f8947520a9b4081318
王子齊 <126648456+tzuch23@users.noreply.github.com>
2023-06-06 23:14:44 +0800

    Update Maze.cpp

--- Maze.cpp
+++ Maze.cpp
@@ -125,11 +125,11 @@ void Maze::print() {
     for (int y = -1; y < HEIGHT + 1; y++) {
         for (int x = -1; x < WIDTH + 1; x++) {
             if (x == -1 || x == WIDTH || y == -1 || y == HEIGHT) {
-                cout << "██";
+                cout << "OO";
             } else {
                 switch (maze[x][y]) {
                     case WALL:
-                        cout << "██";
+                        cout << "OO";
                         break;
                     case PATH:
                         cout << "  ";
